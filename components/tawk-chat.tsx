2882ef92f5fb91ec740c07956a8787d4da756019
Your Name <urielparavi75@gmail.com>
2025-12-26 23:18:47 +0200

    commit

--- components/tawk-chat.tsx
+++ components/tawk-chat.tsx
@@ -44,7 +44,9 @@ export function TawkChat() {
       s1.charset = 'UTF-8';
       s1.setAttribute('crossorigin', '*');
 
-      s0.parentNode.insertBefore(s1, s0);
+      if (s0 && s0.parentNode) {
+        s0.parentNode.insertBefore(s1, s0);
+      }
     })();
   }, []);
 
