7b2a9a7c648b90be4b4f1c7e18adb04619bad7c7
Your Name <urielparavi75@gmail.com>
2026-01-01 13:03:22 +0200

    commit

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: 'https://www.uriel-sites.com/sitemap.xml',
+    sitemap: 'https://uriel-sites.com/sitemap.xml',
   };
 }
